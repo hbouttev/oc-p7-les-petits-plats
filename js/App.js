74739393d72e43ae371fb999ac0dd8f0ab544b2c
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-07-04 16:58:58 +0200

    feat: add wip skeleton for App entry point

---
+++ js/App.js
@@ -0,0 +1,14 @@
+import SearchEngine from "./search/SearchEngine.js";
+import FilterTagDropdown from "./templates/FilterTagDropdown.js";
+
+export default class App {
+  #searchEngine;
+
+  constructor() {}
+
+  /**
+   * Main function of the app, entry point
+   * @returns {Promise<void>}
+   */
+  async main() {}
+}
