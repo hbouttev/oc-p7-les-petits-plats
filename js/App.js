7984c240a3bf065f4b2b750e6de210746d116f2c
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-07-08 19:56:45 +0200

    refactor: move app initialization in App

--- js/App.js
+++ js/App.js
@@ -1,14 +1,30 @@
-import SearchEngine from "./search/SearchEngine.js";
-import FilterTagDropdown from "./templates/FilterTagDropdown.js";
+import { RecipesApi } from "./api/Api.js";
+import Recipe from "./models/Recipe.js";
+import RecipeCardsList from "./templates/RecipeCardsList.js";
 
 export default class App {
-  #searchEngine;
+  #recipesApi;
+  #recipes;
+  #dropdownsFiltersContainer;
+  #searchtagsContainer;
+  #recipesContainer;
 
-  constructor() {}
+  constructor() {
+    this.#recipesApi = new RecipesApi("data/recipes.json");
+    this.#recipes = [];
+    this.#dropdownsFiltersContainer = document.querySelector(".dropdowns-filters-list-container");
+    this.#searchtagsContainer = document.querySelector(".searchtags-list-container");
+    this.#recipesContainer = document.querySelector(".recipes-list-container");
+  }
 
   /**
    * Main function of the app, entry point
    * @returns {Promise<void>}
    */
-  async main() {}
+  async main() {
+    const recipesData = await this.#recipesApi.getAllRecipes();
+    const recipes = recipesData.map((recipe) => new Recipe(recipe));
+    const recipeCardsList = new RecipeCardsList(recipes);
+    this.#recipesContainer.appendChild(recipeCardsList.element);
+  }
 }
