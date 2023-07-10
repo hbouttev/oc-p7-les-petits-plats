5a64e72c24e2db7aa40e8710b50735071a55a7a6
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-07-10 16:36:53 +0200

    feat: add proper FilterTagDropdown component with basic filter (WIP)

--- js/templates/FilterTagDropdown.js
+++ js/templates/FilterTagDropdown.js
@@ -1,4 +1,4 @@
-import { htmlToElement } from "../lib/utils.js";
+import { htmlToElement, removeAllChildNodes } from "../lib/utils.js";
 import PubSub from "../events/PubSub.js";
 import { SearchEventsTypes } from "../events/searchEvents.js";
 
@@ -6,16 +6,34 @@ import { SearchEventsTypes } from "../events/searchEvents.js";
 // l'implémentation du dropdown comme composant
 
 export default class FilterTagDropdown {
+  #filter;
   #options = [];
+  #activeOptions = [];
+  #filteredOptions = [];
+  #optionsWithActive = [];
   #element = null;
+  #searchInput = null;
+  #activeOptionsContainer = null;
+  #filteredOptionsContainer = null;
 
-  constructor(displayName, id) {
+  /**
+   * @param {Filter} filter is a Filter object storing title and id
+   */
+  constructor(filter) {
+    this.#filter = filter;
     this.#initialize();
   }
 
   #initialize() {
     // ajouter les this.addEventListener pour les events custom et les this.dispatchEvent pour les events custom
     this.#createFilterTagDropdown();
+    // test
+    this.#updateOptions(["chocolat", "poulet", "citron", "fromage"]);
+    this.#renderFilteredOptions();
+    this.#searchInput.addEventListener(
+      "input",
+      this.handleFilterInput.bind(this)
+    );
     PubSub.subscribe(
       SearchEventsTypes.UpdateFilterOptions,
       this.handleUpdateFilterOptions.bind(this)
@@ -24,10 +42,51 @@ export default class FilterTagDropdown {
 
   #createFilterTagDropdown() {
     const filterTagDropdown = `
-      <div class="filter-tag-dropdown">
+      <div class="btn-group">
+        <button
+          class="btn btn-primary dropdown-toggle dropdown-filter"
+          type="button"
+          data-bs-toggle="dropdown"
+          data-bs-auto-close="outside"
+          aria-expanded="false"
+        >
+          ${this.#filter.title}
+        </button>
+        <div class="dropdown-menu dropdown dropdown-filter">
+          <div class="px-4 py-3">
+            <input type="search" class="form-control" aria-label="Search" />
+          </div>
+          <div class="active-options">
+            <button class="dropdown-item active" type="button">
+              Dropdown item
+            </button>
+            <button class="dropdown-item active" type="button">
+              Dropdown item
+            </button>
+          </div>
+          <div class="filtered-options">
+          </div>
+        </div>
       </div>
-     `;
+    `;
     this.#element = htmlToElement(filterTagDropdown);
+    this.#activeOptionsContainer =
+      this.#element.querySelector(".active-options");
+    this.#filteredOptionsContainer =
+      this.#element.querySelector(".filtered-options");
+    this.#searchInput = this.#element.querySelector("input");
+  }
+
+  #filterOptions(filter) {
+    this.#filteredOptions = this.#options.filter((option) =>
+      option.toLowerCase().includes(filter)
+    );
+  }
+
+  handleFilterInput() {
+    const filter = this.#searchInput.value.toLowerCase();
+    this.#filterOptions(filter);
+    this.#renderFilteredOptions();
   }
 
   /**
@@ -36,7 +95,24 @@ export default class FilterTagDropdown {
    */
   #updateOptions(newOptions) {
     this.#options = newOptions;
+    this.#optionsWithActive = newOptions.filter(
+      (newOption) => !this.#activeOptions.includes(newOption)
+    );
+    this.handleFilterInput();
+
     // this.#renderOptions();
+  }
+
+  #renderActiveOptions() {}
+
+  #renderFilteredOptions() {
+    removeAllChildNodes(this.#filteredOptionsContainer);
+    this.#filteredOptions.forEach((option) => {
+      const optionElement = htmlToElement(`
+        <button class="dropdown-item" type="button">${option}</button>
+      `);
+      this.#filteredOptionsContainer.appendChild(optionElement);
+    });
   }
 
   /**
@@ -46,5 +122,9 @@ export default class FilterTagDropdown {
    */
   handleUpdateFilterOptions(event, newOptions) {
     this.#updateOptions(newOptions);
+  }
+
+  get element() {
+    return this.#element;
   }
 }
