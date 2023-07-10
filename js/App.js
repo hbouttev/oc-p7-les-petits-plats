5013f742de4445ab4cd9a1cc366b621246ccf88f
Hadrien Boutteville <h.boutteville@leosidea.com>
2023-07-10 16:39:06 +0200

    feat: update App no generate filter dropdowns

--- js/App.js
+++ js/App.js
@@ -1,6 +1,9 @@
 import { RecipesApi } from "./api/Api.js";
 import Recipe from "./models/Recipe.js";
 import RecipeCardsList from "./templates/RecipeCardsList.js";
+import { Filters } from "./search/SearchEngine.js";
+import Filter from "./models/Filter.js";
+import FilterTagDropdownsList from "./templates/FilterTagDropdownsList.js";
 
 export default class App {
   #recipesApi;
@@ -12,8 +15,12 @@ export default class App {
   constructor() {
     this.#recipesApi = new RecipesApi("data/recipes.json");
     this.#recipes = [];
-    this.#dropdownsFiltersContainer = document.querySelector(".dropdowns-filters-list-container");
-    this.#searchtagsContainer = document.querySelector(".searchtags-list-container");
+    this.#dropdownsFiltersContainer = document.querySelector(
+      ".dropdowns-filters-list-container"
+    );
+    this.#searchtagsContainer = document.querySelector(
+      ".searchtags-list-container"
+    );
     this.#recipesContainer = document.querySelector(".recipes-list-container");
   }
 
@@ -25,6 +32,11 @@ export default class App {
     const recipesData = await this.#recipesApi.getAllRecipes();
     const recipes = recipesData.map((recipe) => new Recipe(recipe));
     const recipeCardsList = new RecipeCardsList(recipes);
+    const filters = Object.values(Filters).map(
+      ({ displayName, id }) => new Filter(displayName, id)
+    );
+    const filterTagDropdownsList = new FilterTagDropdownsList(filters);
+    this.#dropdownsFiltersContainer.appendChild(filterTagDropdownsList.element);
     this.#recipesContainer.appendChild(recipeCardsList.element);
   }
 }
